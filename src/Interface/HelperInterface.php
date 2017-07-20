c25ae8c73b2e2375af93fd77d932309fb3289926
Andrea <andrea.tantimonaco@outlook.com>
2017-07-20 18:12:59 +0200

    A basic HelperInterface

---
+++ src/Interface/HelperInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AndrewReborn\Mediatoolkit\Interfaces;
+
+use GuzzleHttp\Client;
+
+/**
+ * HelperInterface
+ */
+interface HelperInterface
+{
+    public function __construct(Client $client, $organisation, $token);
+}
