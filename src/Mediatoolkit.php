c8e2e4e3fb96e8722fe1d85ba5ab5207e3936608
Andrea <andrea.tantimonaco@outlook.com>
2017-07-20 18:18:02 +0200

    SDK entry point

---
+++ src/Mediatoolkit.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace AndrewReborn\Mediatoolkit;
+
+use AndrewReborn\Mediatoolkit\Exceptions\MediatoolkitException,
+    AndrewReborn\Mediatoolkit\Helpers\GroupHelper;
+
+class Mediatoolkit
+{
+    const BASE_URI               = 'https://api.mediatoolkit.com';
+    const TOKEN_ENV_NAME         = 'MEDIATOOLKIT_API_TOKEN';
+    const TOKEN_ENV_ORGANISATION = 'MEDIATOOLKIT_API_ORGANISATION';
+    
+    /**
+     * The organization Id
+     *
+     * @var string
+     */
+    private $_organisation;
+
+    /**
+     * A Token is required to authorize each request
+     *
+     * @var string
+     */
+    private $_token;
+    
+    /**
+     * The Guzzle HTTP Client
+     *
+     * @var GuzzleHttp\Client
+     */
+    protected $client;
+
+    /**
+     * Initialize the Mediatoolkit SDK
+     */
+    public function __construct($organisation = null, $token = null)
+    {
+        $this->_organisation = getenv(self::ORGANISATION_ENV_NAME) ?? $organisation;
+        $this->_token        = getenv(self::TOKEN_ENV_NAME) ?? $token;
+
+        if (empty($this->_organisation)) {
+            throw new MediatoolkitException('No Organisation provided');
+        }
+
+        if (empty($this->_token)) {
+            throw new MediatoolkitException('No Token provided');
+        }
+
+        $this->client = new Client(['base_uri' => self::BASE_URI]);
+    }
+
+    /**
+     * Returns an instance of the GroupHelper that provides the Group CRUD endpoints
+     *
+     * @return GroupHelper
+     */
+    public function getGroupHelper(): GroupHelper
+    {
+        return new GroupHelper($this->client, $this->_organisation, $this->_token);
+    }
+
+    /**
+     * Returns an instance of the KeywordHelper that provides the Keyword CRUD endpoints
+     *
+     * @return GroupHelper
+     */
+    public function getKeywordHelper(): KeywordHelper
+    {
+        return new KeywordHelper($this->client, $this->_organisation, $this->_token);
+    }
+
+    /**
+     * Returns the current Organization
+     * 
+     * @return string
+     */
+    public function getOrganization()
+    {
+        return $this->_organization;
+    }
+
+    /**
+     * Returns the current Token
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->_token;
+    }
+}
