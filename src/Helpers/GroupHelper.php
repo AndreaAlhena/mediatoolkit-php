2ff598cd70978d240cfe65dd4e9e14983a3632e8
Andrea <andrea.tantimonaco@outlook.com>
2017-07-24 10:28:26 +0200

    Create method

--- src/Helpers/GroupHelper.php
+++ src/Helpers/GroupHelper.php
@@ -6,29 +6,44 @@ use Mediatoolkit\Models\Group;
 
 class GroupHelper extends Helper
 {
-    const RESPONSE_GROUPS_KEY = 'groups';
+    const RESPONSE_GROUPS_KEY       = 'groups';
+
+    public function create(string $name, bool $isPublic = true): Group
+    {
+        $response = $this->request(
+            'groups',
+            [
+                Group::DATA_IS_PUBLIC_KEY => $isPublic,
+                Group::DATA_NAME_KEY      => $name
+            ],
+            'post'
+        );
+
+        $data  = $response->getData();
+        return new Group($data);
+    }
+
+    public function find(int $group): Group
+    {
+
+    }
 
     /**
-     * Returns the available groups. If a $group is provided, a Group is returned.
-     * Otherwise, a stack of Groups is provided
+     * Returns the available groups
      *
-     * @param string $group The Group identifier
-     * 
-     * @return mixed Group | array
+     * @return mixed array An array with the available Group models
      */
-    public function read($group = null)
+    public function read($group = null): array
     {
-        if (empty($group)) {
-            $response = $this->request('groups', []);
+        $response = $this->request('groups', []);
 
-            $data   = $response->getData();
-            $groups = [];
+        $data   = $response->getData();
+        $groups = [];
 
-            foreach ($data[self::RESPONSE_GROUPS_KEY] as $group) {
-                $groups[] = new Group($group);
-            }
-
-            return $groups;
+        foreach ($data[self::RESPONSE_GROUPS_KEY] as $group) {
+            $groups[] = new Group($group);
         }
+
+        return $groups;
     }
 }
